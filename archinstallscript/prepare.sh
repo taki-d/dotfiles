4c51fb9cbebd0f097c10b0a1029c8366f85f4e04
taki-d <taka410199@outlook.jp>
2017-02-17 20:16:27 +0900

    add archinstall script

---
+++ archinstallscript/prepare.sh
@@ -0,0 +1,61 @@
+#!bin/bash
+
+function connect_to_network(){
+
+    wifi-menu
+
+    ping goo.gl -c 1 >> /dev/null
+
+    if [ $? -eq 0 ];
+    then
+        echo "0" # successed
+    else
+        echo "1" # faild
+    fi
+
+}
+
+function prepare(){
+
+    loadkeys jp106
+
+    timedatectl set-ntp true
+
+    sed -e "1i Server = http://ftp.tsukuba.wide.ad.jp/Linux/archlinux/$repo/os/$arch" /etc/pacman.d/mirrorlist
+    sed -e "2i Server = http://ftp.jaist.ac.jp/pub/Linux/ArchLinux/$repo/os/$arch" /etc/pacman.d/mirrorlist
+    
+}
+
+status="0"
+
+echo "Please Connect to Network"
+
+if [ "`connect_to_network`" = "1" ];
+then
+    echo "error: Cannnot Connect to the Internet"
+    status="1"
+else
+    echo ""
+fi
+
+if [ "${status}" = "0" ];
+then
+    prepare
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
