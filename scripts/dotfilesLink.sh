fd56a6822b0f88881e9ebe6035daee2e97143dc7
taki-d <taka410199@gmail.com>
2019-07-20 15:10:06 +0900

    [update]update font settings

--- scripts/dotfilesLink.sh
+++ scripts/dotfilesLink.sh
@@ -7,3 +7,6 @@ ln -sf ~/dotfiles/.config ~/nvim
 ln -sf ~/dotfiles/.xprofile ~/.xprofile
 ln -sF ~/dotfiles/.Xresources ~/.Xresources
 ln -sf ~/dotfiles/.Xdefaults ~/.Xdefaults
+ln -sf ~/dotfiles/etc/gtk/gtkrc-2.0 ~/.gtkrc-2.0
+ln -sf ~/dotfiles/etc/gtk/settings.ini ${XDG_CONFIG_HOME}/gtk-3.0/settings.ini
+
