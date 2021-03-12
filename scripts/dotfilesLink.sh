9c163e8e3060dceba98e9d0e757be2d9e12ab9e6
taki-d <taka410199@gmail.com>
2021-03-12 19:07:59 +0900

    [add]add all

--- scripts/dotfilesLink.sh
+++ scripts/dotfilesLink.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
+DOTFILES_HOME=~/dotfiles
+XDG_CONFIG_HOME=~/.config
 
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/vim ~/.vim
-ln -sf ~/dotfiles/.config ~/nvim
-ln -sf ~/dotfiles/.xprofile ~/.xprofile
-ln -sF ~/dotfiles/.Xresources ~/.Xresources
-ln -sf ~/dotfiles/.Xdefaults ~/.Xdefaults
-ln -sf ~/dotfiles/etc/gtk/gtkrc-2.0 ~/.gtkrc-2.0
-ln -sf ~/dotfiles/etc/gtk/settings.ini ${XDG_CONFIG_HOME}/gtk-3.0/settings.ini
-ln -sf ~/dotfiles/etc/qt/Trolltech.conf ${XDG_CONFIG_HOME}/Trolltech.conf
-ln -sf ~/dotfiles/etc/xorg.conf.d/70-synaptics.conf /etc/X11/xorg.conf.d/70-synaptics.conf
-
+ln -sf ${DOTFILES_HOME}/pkgs/zsh/zshrc ~/.zshrc
+ln -sf ${DOTFILES_HOME}/pkgs/x/xprofile ~/.xprofile
+ln -sf ${DOTFILES_HOME}/pkgs/x/Xresources ~/.Xresources
+ln -sf ${DOTFILES_HOME}/pkgs/x/Xdefaults ~/.Xdefaults
+ln -sf ${DOTFILES_HOME}/pkgs/gtk/gtkrc-2.0 ~/.gtkrc-2.0
+ln -sf ${DOTFILES_HOME}/pkgs/gtk/settings.ini ${XDG_CONFIG_HOME}/gtk-3.0/settings.ini
+ln -sf ${DOTFILES_HOME}/pkgs/qt/Trolltech.conf ${XDG_CONFIG_HOME}/Trolltech.conf
+ln -sf ${DOTFILES_HOME}/pkgs/i3/i3-config ${XDG_CONFIG_HOME}/i3/config
+ln -sf ${DOTFILES_HOME}/pkgs/i3/memory.py ${XDG_CONFIG_HOME}/i3/memory.py
+ln -sf ${DOTFILES_HOME}/pkgs/i3/i3blocks-config ${XDG_CONFIG_HOME}/i3blocks/config
+ln -sf ${DOTFILES_HOME}/pkgs/alacritty/alacritty.yml ~/.alacritty.yml
+ln -sf ${DOTFILES_HOME}/pkgs/font/fontconfig ${XDG_CONFIG_HOME}/fontconfig/fonts.conf
