497b4d98f85e3a983461c70b1d6ea87c056e1b0d
Takahiro <mail@orangepla.net>
2022-06-13 09:18:36 +0900

    [add] add all

---
+++ pkgs/zsh/zsh.conf.d/10-zi-plugin.zsh
@@ -0,0 +1,14 @@
+export ZI_HOME_DIR="$HOME/.zi"
+
+if [[ ! -d "${ZI_HOME_DIR}" ]]; then
+    git clone https://github.com/z-shell/zi.git "$ZI_HOME_DIR"
+fi
+source $ZI_HOME_DIR/zi.zsh
+
+autoload -Uz _zi
+
+
+zi snippet 'OMZL::completion.zsh'
+
+
+
