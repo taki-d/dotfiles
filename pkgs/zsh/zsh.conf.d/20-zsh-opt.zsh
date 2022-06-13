67b30a50d7c1893bd86db6cffd872b76f6bb88f8
Takahiro <mail@orangepla.net>
2022-06-13 09:21:50 +0900

    [add]

---
+++ pkgs/zsh/zsh.conf.d/20-zsh-opt.zsh
@@ -0,0 +1,11 @@
+
+setopt hist_ignore_dups
+setopt hist_ignore_all_dups
+setopt extended_history
+setopt share_history
+setopt append_history
+setopt inc_append_history
+setopt hist_no_store
+setopt hist_reduce_blanks
+setopt autocd
+setopt correct
