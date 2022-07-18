68389f69e3b953f809110fc5e99dc8a802539b4b
taki-d <mail@orangepla.net>
2022-07-18 17:37:13 +0900

    [update] update placement script

--- scripts/dotfilesLink.sh
+++ scripts/dotfilesLink.sh
@@ -14,11 +14,18 @@ if [[ "$(uname -r)" != *microsoft* ]]; then
     ln -sf ${DOTFILES_HOME}/pkgs/gtk/gtkrc-2.0 ~/.gtkrc-2.0
     ln -sf ${DOTFILES_HOME}/pkgs/gtk/settings.ini ${XDG_CONFIG_HOME}/gtk-3.0/settings.ini
     ln -sf ${DOTFILES_HOME}/pkgs/qt/Trolltech.conf ${XDG_CONFIG_HOME}/Trolltech.conf
+
+    mkdir -p ${XDG_CONFIG_HOME}/i3
+    mkdir -p ${XDG_CONFIG_HOME}/i3blocks
+
     ln -sf ${DOTFILES_HOME}/pkgs/i3/i3-config ${XDG_CONFIG_HOME}/i3/config
     ln -sf ${DOTFILES_HOME}/pkgs/i3/memory.py ${XDG_CONFIG_HOME}/i3/memory.py
     ln -sf ${DOTFILES_HOME}/pkgs/i3/i3blocks-config ${XDG_CONFIG_HOME}/i3blocks/config
-    ln -sf ${DOTFILES_HOME}/pkgs/font/fontconfig ${XDG_CONFIG_HOME}/fontconfig/fonts.conf
-    ln -sf ${DOTFILES_HOME}/pkgs/font/71-no-embeded-bitmaps.conf /etc/fonts/conf.d/71-no-embedded-bitmaps.conf
+    
+    mkdir -p ${XDG_CONFIG_HOME}/fontconfig/conf.d/
+    
+    ln -sf ${DOTFILES_HOME}/pkgs/font/fonts.conf ${XDG_CONFIG_HOME}/fontconfig/fonts.conf
+    ln -sf ${DOTFILES_HOME}/pkgs/font/71-no-embeded-bitmaps.conf ${XDG_CONFIG_HOME}/fontconfig/conf.d/71-no-embedded-bitmaps.conf
 fi
 
 ln -sf ${DOTFILES_HOME}/pkgs/fish ~/.config/fish
