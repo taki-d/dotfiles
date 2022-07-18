b538c45070d2b99ab2e22e9bacd62562025372b3
taki-d <mail@orangepla.net>
2022-07-18 17:34:13 +0900

    [add] add ailias for lab display setup

--- pkgs/zsh/zsh.conf.d/50-alias.zsh
+++ pkgs/zsh/zsh.conf.d/50-alias.zsh
@@ -5,3 +5,4 @@ alias egrep='egrep --color=auto'
 # /home/taki/Screenshot/hogedate.pngに保存するように設定．
 alias gnome-screenshot="gnome-screenshot -f $HOME/Screenshot/$(date +%Y-%m-%d-%H:%M:%S).png"
 alias n=nvim
+alias lab='xrandr --output DP3 --above eDP1 --mode 3840x1600 && xrandr --output eDP1 --off'
