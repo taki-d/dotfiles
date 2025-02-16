211f49d831cc8721c2466c69b996f855315be644
taki-d <mail@orangepla.net>
2025-02-17 01:00:27 +0900

    update

--- pkgs/zsh/zsh.conf.d/10-zi-plugin.zsh
+++ pkgs/zsh/zsh.conf.d/10-zi-plugin.zsh
@@ -10,7 +10,10 @@ autoload -Uz _zi
 zi snippet 'OMZL::completion.zsh'
 
 zi light b4b4r07/enhancd
-zi light zsh-users/zsh-completions
 zi light zsh-users/zsh-autosuggestions
+zi light zsh-users/zsh-completions
 zi light zsh-users/zsh-syntax-highlighting
 zi light chrissicool/zsh-256color
+
+zi light MichaelAquilina/zsh-you-should-use
+# zi light sbugzu/gruvbox-zsh
