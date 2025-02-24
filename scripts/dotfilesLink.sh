e5082f73dbc58e0ba552202f7d7a2162a94d69cc
taki-d <mail@orangepla.net>
2025-02-25 00:59:58 +0900

    update: enable ShellCheck in VSCode settings and clean up dotfilesLink script

--- scripts/dotfilesLink.sh
+++ scripts/dotfilesLink.sh
@@ -28,12 +28,12 @@ elif [[ "$(expr substr "$(uname -s)" 1 5)" == "Linux" ]]; then
     ln -sf ${DOTFILES_HOME}/pkgs/i3/i3-config ${XDG_CONFIG_HOME}/i3/config
     ln -sf ${DOTFILES_HOME}/pkgs/i3/memory.py ${XDG_CONFIG_HOME}/i3blocks/memory.py
     ln -sf ${DOTFILES_HOME}/pkgs/i3/i3blocks-config ${XDG_CONFIG_HOME}/i3blocks/config
-    
+
     mkdir -p ${XDG_CONFIG_HOME}/fontconfig/conf.d/
-    
+
     ln -sf ${DOTFILES_HOME}/pkgs/font/fonts.conf ${XDG_CONFIG_HOME}/fontconfig/fonts.conf
     ln -sf ${DOTFILES_HOME}/pkgs/font/71-no-embeded-bitmaps.conf ${XDG_CONFIG_HOME}/fontconfig/conf.d/71-no-embedded-bitmaps.conf
-    
+
     ln -sf ${DOTFILES_HOME}/pkgs/vscode/settings.json ${XDG_CONFIG_HOME}/Code/User/settings.json
 fi
 
