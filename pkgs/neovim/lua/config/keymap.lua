76fd200b545c0de233253bf635246df6af06ef3e
taki-d <mail@orangepla.net>
2025-05-19 23:42:35 +0900

    update: add key mapping for opening explorer in VS Code

--- pkgs/neovim/lua/config/keymap.lua
+++ pkgs/neovim/lua/config/keymap.lua
@@ -10,6 +10,12 @@ if vim.g.vscode then
 			vscode.action("editor.action.rename")
 		end)
 	end, { desc = "VS Code Rename Symbol" })
+
+	vim.keymap.set("n", "<leader>e", function()
+		vscode.with_insert(function()
+			vscode.action("workbench.view.explorer")
+		end)
+	end, { desc = "VS Code open explorer" })
 else
 	-- normal neovim
 end
