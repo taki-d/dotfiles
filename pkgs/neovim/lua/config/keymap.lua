c94b85c21b12c77d2c2e91429fb9c62cf0d86a03
taki-d <mail@orangepla.net>
2025-05-19 23:01:59 +0900

    update: add key mapping for formatting in VSCode environment

--- pkgs/neovim/lua/config/keymap.lua
+++ pkgs/neovim/lua/config/keymap.lua
@@ -1,6 +1,15 @@
-
 -- leader key
 vim.g.mapleader = " "
 
+if vim.g.vscode then
+	-- vscode
 
-
+	vim.keymap.set(
+		{ "n", "v" },
+		"<leader>f",
+		"<cmd>lua vim.lsp.buf.format({ async = true })<CR>",
+		{ desc = "Format" }
+	)
+else
+	-- normal neovim
+end
