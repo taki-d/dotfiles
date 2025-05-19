312a0f63603be3658961e0e1c23f2acf1dd87e3a
taki-d <mail@orangepla.net>
2025-05-19 23:25:10 +0900

    Add: <leader>rn -> refactor.rename

--- pkgs/neovim/lua/config/keymap.lua
+++ pkgs/neovim/lua/config/keymap.lua
@@ -1,15 +1,15 @@
 -- leader key
 vim.g.mapleader = " "
 
+local visualStudioCode = require("vscode")
 if vim.g.vscode then
 	-- vscode
 
-	vim.keymap.set(
-		{ "n", "v" },
-		"<leader>f",
-		"<cmd>lua vim.lsp.buf.format({ async = true })<CR>",
-		{ desc = "Format" }
-	)
+	vim.keymap.set({ "n", "v" }, "<leader>rn", function()
+		visualStudioCode.with_insert(function()
+			visualStudioCode.action("editor.action.rename")
+		end)
+	end, { desc = "VS Code Rename Symbol" })
 else
 	-- normal neovim
 end
