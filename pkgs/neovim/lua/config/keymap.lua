414283813a0358b91877d2d4b4cba4532012deba
taki-d <mail@orangepla.net>
2025-05-19 23:26:12 +0900

    fix: correct variable name for Visual Studio Code integration

--- pkgs/neovim/lua/config/keymap.lua
+++ pkgs/neovim/lua/config/keymap.lua
@@ -1,13 +1,13 @@
 -- leader key
 vim.g.mapleader = " "
 
-local visualStudioCode = require("vscode")
+local vscode = require("vscode")
 if vim.g.vscode then
 	-- vscode
 
 	vim.keymap.set({ "n", "v" }, "<leader>rn", function()
-		visualStudioCode.with_insert(function()
-			visualStudioCode.action("editor.action.rename")
+		vscode.with_insert(function()
+			vscode.action("editor.action.rename")
 		end)
 	end, { desc = "VS Code Rename Symbol" })
 else
