a4a3e439365179828ef23ee378869b61b4a23b46
taki-d <mail@orangepla.net>
2025-05-19 23:31:42 +0900

    fix: move vscode require statement inside the conditional block

--- pkgs/neovim/lua/config/keymap.lua
+++ pkgs/neovim/lua/config/keymap.lua
@@ -1,9 +1,9 @@
 -- leader key
 vim.g.mapleader = " "
 
-local vscode = require("vscode")
 if vim.g.vscode then
 	-- vscode
+	local vscode = require("vscode")
 
 	vim.keymap.set({ "n", "v" }, "<leader>rn", function()
 		vscode.with_insert(function()
